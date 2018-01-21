3a8de659e569278336dc03385df892fcf5a9bb0c
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-21 10:03:30 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("enter any alphabet");
+scanf("%c",&ch);
+if((ch>=65&&ch<=0)
+{
+printf("enter any alphabet");
+}
+else
+printf("enter character is not alphabet");
+}
+getch();
+}
