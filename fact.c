240abf15c60ba8a5e49694397ec4ad4b90c40a57
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-21 11:54:33 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,fact=1,i;
+printf("enter any integer");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+fact=fact x;
+}
+5!
+printf("factorioal of given number is:%d",fact);
+return 0;
+}
