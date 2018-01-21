106a2c4c3a39e1e19339580d2624063da36ad31f
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-21 09:48:07 +0530

    Rename even.c to odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter any integer");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("given number is even");
+}
+else
+{
+printf("given number is odd");
+}
+return 0;
+}
