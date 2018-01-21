3f2bed17d8242e21cdd0686475d4ce345f8e543d
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-21 09:42:11 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i;
+printf("enter any integer");
+scanf("%d",&n);
+if(n<=0)
+{
+(if(n==0)
+{
+printf("given number is zero");
+}
+else
+{
+printf("given number is integer");
+}
+}
+else
+{
+printf("enter the number is positive");
+}
+return 0;
+}
