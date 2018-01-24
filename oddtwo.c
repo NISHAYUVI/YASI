fd9011de89d2bb2133f09354755482032a55ee85
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-24 16:31:50 +0530

    Update oddtwo.c

--- oddtwo.c
+++ oddtwo.c
@@ -6,14 +6,14 @@ printf("enter the number");
 scanf("%d",&n);
 scanf("%d",&k);
 for(i=n;i,=k;i++)
-{
+{e
 if(i%2==0)
-{
+{e
 e[j]=i;
 j++;e
 }
 else
-{
+{e
 0[k]=i;
 k++;
 }
@@ -23,5 +23,5 @@ printf("%d",e[i]);
 }
 return 0;
 }
-e
+
 
