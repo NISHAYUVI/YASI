e69e707b40620edbf2d3e3fd0557277908cff51d
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-01-24 16:22:25 +0530

    Create oddtwo.c

---
+++ oddtwo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,j=0,k=0;
+printf("enter the number");
+scanf("%d",&n);
+scanf("%d",&k);
+for(i=n;i,=k;i++)
+{
+if(i%2==0)
+{
+
+e[j]=i;
+
+
+j++;
+}
+else
+{
+0[k]=i;
+k++;
+}
+for(i=0;i<j;i++)
+{
+printf("%d",e[i]);
+
+}
+return 0;
+}
+
+
