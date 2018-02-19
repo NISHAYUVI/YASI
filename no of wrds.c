89870a079e3550dde559dee9ed6b33c9b336601b
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-02-19 16:02:46 +0530

    Create no of wrds.c

---
+++ no of wrds.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int count=0,i;
+printf("enter the string");
+scanf("%d",&b);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+count++;
+}
+printf("number of words:%d",count+1);
+getch();
+}
