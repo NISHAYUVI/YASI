527aeb681601ef3c55385a92ad360936554d6bae
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-02-26 11:00:35 +0000

    Create concanate.c

---
+++ concanate.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+{
+    char i,j,k;
+    printf("\nenter the first string");
+    scanf("%c",&i);
+    printf("\nenter the second string");
+    scanf("%c",&j);
+    k= i,j;
+    printf("\nthe concatenate string is%c",k);
+retun 0;
+}
