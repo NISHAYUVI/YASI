14aedef609d49ca5f2b6df571affedc6c89eecdc
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-02-26 10:50:47 +0000

    Create sring.c

---
+++ sring.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[30];
+int i,k;
+printf("enter the string ");
+scanf("%d",&a);
+printf(" enter how many time want to print ");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+	printf("%d",a);
+}
+return 0;
+}
