3be7eb76d933fae19526450ca105aaf02ae8f93c
NISHAYUVI <31503949+NISHAYUVI@users.noreply.github.com>
2018-03-19 10:31:44 +0000

    Create strngbinary.c

---
+++ strngbinary.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int s[50],i;
+    printf("\n enter the string :");
+    scanf("%d",&s[i]);
+   if(s[i]=='0'||'1')
+   {
+     printf("yes");
+   }
+    else
+    {
+printf("no");
+}
+return 0;
+}
